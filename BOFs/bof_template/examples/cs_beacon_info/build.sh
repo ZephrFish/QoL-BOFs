57fde48f9a8a5ab40578db9112a73fb211fe9f84
ZephrFish <ZephrFish@users.noreply.github.com>
2024-12-12 12:21:55 +0000

    Added submodules for various BOFs and tools

--- BOFs/bof_template/examples/cs_beacon_info/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-i686-w64-mingw32-gcc -c cs_beacon_info.c -o cs_beacon_info.x86.o
-x86_64-w64-mingw32-gcc -c cs_beacon_info.c -o cs_beacon_info.x64.o
--- BOFs/bofhound/bofhound/ad/__init__.py
+++
@@ -1,1 +0,0 @@
-from .adds import ADDS
