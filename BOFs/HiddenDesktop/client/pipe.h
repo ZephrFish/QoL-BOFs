d97edf47ee57e5ae54b52229b27443239a07a562
ZephrFish <ZephrFish@users.noreply.github.com>
2024-12-12 12:08:12 +0000

    Fixing sub modules

--- BOFs/HiddenDesktop/client/pipe.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-SECTION( E ) HANDLE PipeInit( PAPI pApi, PCHAR Name );
-SECTION( E ) BOOL PipeWait( PAPI pApi, HANDLE Pipe );
-SECTION( E ) BOOL PipePrint( PAPI pApi, HANDLE Pipe, PCHAR Format, ... );
